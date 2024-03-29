3bd328721a1d4663948f4837e60cf226155add7c
maababa <ujjwal1899@gmail.com>
2024-03-30 02:10:12 +0530

    hareKrishna

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import Collection from '@/components/shared/Collection';
 import { Button } from '@/components/ui/button'
+import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
+
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  // const page = Number(searchParams?.page) || 1;
+  // const searchText = (searchParams?.query as string) || '';
+  // const category = (searchParams?.category as string) || '';
+
+  const events = await getAllEvents({
+    query: '',
+    category:'',
+    page:1,
+    limit: 6
+  })
 
   return (
     <>
@@ -27,6 +41,25 @@ export default async function Home() {
           />
         </div>
       </section> 
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          Search
+          CategoryFilter
+        </div>
+
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={2}
+          totalPages={2}
+        />
+      </section>
     </>
   )
 }
